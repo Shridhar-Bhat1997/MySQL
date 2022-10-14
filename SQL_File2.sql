7febcfd6afe3452248b9dd76c9a049e9f9c02326
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-10-14 18:50:57 +0530

    Create Table statement (MySQL) 14-Oct-2022

---
+++ SQL_File2.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE sample;
+
+CREATE TABLE employee # To create table
+(
+empid int primary key,
+name varchar(30),
+dob datetime,
+email varchar(20)
+);
+
+DESC employee; # DESCRIBE command is used to dipslay the information about a table.
