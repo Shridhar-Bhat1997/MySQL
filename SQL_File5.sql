aec6e95e4ccc84da45aa4ed935505119e6f346a0
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-10-14 19:31:58 +0530

    Inserting Data into Tables (MySQL) 14-Oct-2022

---
+++ SQL_File5.sql
@@ -0,0 +1,15 @@
+DESC employee;
+
+# inserting data  tables
+
+INSERT INTO employee (empid,name,dob,email)
+VALUES (1001,"Mahi","1998-03-12 11:55:00","mahi@gmail.com");
+
+INSERT INTO employee
+VALUES (1002,"ABC","1999-04-23 03:52:13","abc@gmail.com");
+
+SELECT * FROM employee; # to fetch the data from the table
+
+
+
+
